58d645c4c3b5f12d01cfef00c29f37cfe267174c
Trock <g_trock@163.com>
2021-08-20 16:52:49 +0800

    refactor: just with context, add redis

--- backend/firestore/firestore.go
+++ backend/firestore/firestore.go
@@ -3,43 +3,57 @@ package firestore
 import (
 	"context"
 	"errors"
+	"github.com/DoNewsCode/crypt/internal"
+	"sync"
 	"time"
 
 	"cloud.google.com/go/firestore"
 	"github.com/DoNewsCode/crypt/backend"
-	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
 )
 
 type Client struct {
-	client *firestore.Client
+	client        *firestore.Client
+	cache         *sync.Map
+	watchInterval time.Duration
 }
 
 type data struct {
 	Data []byte `firestore:"data"`
 }
 
-func New(machines []string) (*Client, error) {
+type OptionFunc func(client *Client)
+
+func WithWatchInterval(duration time.Duration) OptionFunc {
+	return func(client *Client) {
+		client.watchInterval = duration
+	}
+}
+
+func New(machines []string, opts ...OptionFunc) (*Client, error) {
 	if len(machines) == 0 {
 		return nil, errors.New("project should be defined")
 	}
 
-	opts := []option.ClientOption{
+	c, err := firestore.NewClient(context.TODO(), machines[0], []option.ClientOption{
 		option.WithGRPCDialOption(grpc.WithBlock()),
-	}
-	c, err := firestore.NewClient(context.TODO(), machines[0], opts...)
+	}...)
 	if err != nil {
 		return nil, err
 	}
-	return &Client{c}, nil
+	cli := &Client{
+		client:        c,
+		cache:         &sync.Map{},
+		watchInterval: 10 * time.Second,
+	}
+	for _, opt := range opts {
+		opt(cli)
+	}
+	return cli, nil
 }
 
-func (c *Client) Get(path string) ([]byte, error) {
-	return c.GetWithContext(context.TODO(), path)
-}
-
-func (c *Client) GetWithContext(ctx context.Context, path string) ([]byte, error) {
+func (c *Client) Get(ctx context.Context, path string) ([]byte, error) {
 	snap, err := c.client.Doc(path).Get(ctx)
 	if err != nil {
 		return nil, err
@@ -53,66 +67,29 @@ func (c *Client) GetWithContext(ctx context.Context, path string) ([]byte, error
 	return d.Data, nil
 }
 
-func (c *Client) List(collection string) (backend.KVPairs, error) {
-	return c.ListWithContext(context.TODO(), collection)
-}
-
-func (c *Client) ListWithContext(ctx context.Context, collection string) (backend.KVPairs, error) {
-	res := backend.KVPairs{}
-	it := c.client.Collection(collection).Documents(ctx)
-	for {
-		doc, err := it.Next()
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		d := &data{}
-		err = doc.DataTo(&d)
-		if err != nil {
-			return nil, err
-		}
-		res = append(res, &backend.KVPair{
-			Key:   doc.Ref.ID,
-			Value: d.Data,
-		})
-	}
-	return res, nil
-}
-
-func (c *Client) Set(path string, value []byte) error {
-	return c.SetWithContext(context.TODO(), path, value)
-}
-
-func (c *Client) SetWithContext(ctx context.Context, path string, value []byte) error {
+func (c *Client) Set(ctx context.Context, path string, value []byte) error {
 	_, err := c.client.Doc(path).Set(ctx, &data{value})
 	return err
 }
 
-func (c *Client) Watch(path string, stop chan bool) <-chan *backend.Response {
-	return c.WatchWithContext(context.TODO(), path, stop)
-}
-
-func (c *Client) WatchWithContext(ctx context.Context, path string, stop chan bool) <-chan *backend.Response {
+func (c *Client) Watch(ctx context.Context, path string) <-chan *backend.Response {
 	ch := make(chan *backend.Response, 0)
-	t := time.NewTicker(time.Second)
+
 	go func() {
+		defer func() {
+			close(ch)
+		}()
 		for {
 			select {
-			case <-t.C:
-				v := &data{}
-				snap, err := c.client.Doc(path).Get(ctx)
-				if err == nil {
-					err = snap.DataTo(&v)
-				}
-				ch <- &backend.Response{v.Data, err}
+			case <-time.After(c.watchInterval):
+				val, err := c.Get(ctx, path)
 				if err != nil {
-					time.Sleep(time.Second * 5)
+					ch <- &backend.Response{Error: err}
+					continue
 				}
-			case <-stop:
-				close(ch)
+				internal.WatchCache(c.cache, path, val, ch)
+			case <-ctx.Done():
+				ch <- &backend.Response{Error: ctx.Err()}
 				return
 			}
 		}
